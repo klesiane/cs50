292e62e4b6fe394a8f29ad2b3967239026a62e0a
Klesiane Ramos <100094761+klesiane@users.noreply.github.com>
2022-06-03 12:01:17 -0300

    Create mariohard.c

---
+++ mariohard.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //inserir as variaveis
+    int altura;
+    int linhas;
+    int coluna;
+    int espaço;
+
+    do
+    {
+        //perguntar altura entre 1 e 8
+        altura = get_int ("altura: ");
+    }
+    while (altura > 8 || altura < 1);
+
+    for (linhas = 0; linhas < altura ; linhas++)
+    {
+        for (espaço = 0; espaço < altura - linhas - 1; espaço++)
+        {
+            printf(" ");
+        }
+
+        for (coluna = 0; coluna <= linhas; coluna++ )
+        {
+            printf("#");
+        }
+            printf ("  ");
+            for (coluna = 0; coluna<= linhas; coluna++)
+            {
+                printf ("#");
+            }
+            printf ("\n");
+    }
+}
